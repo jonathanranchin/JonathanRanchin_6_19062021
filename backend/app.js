8f28e99f2a4936f57b554143afcd14cf25881eb6
Jonathan Ranchin <jonathan.ranchin@yahoo.com>
2021-06-25 00:14:50 +0200

    Réparation du projet sur Github/Git

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const saucesRoutes = require("./routes/sauce");
+const path = require("path");
+let cors = require("cors");
+const app = express();
+
+mongoose
+  .connect("mongodb+srv://admin:PASSWORD@p6cluster.zuvde.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
+
+module.exports = app;
