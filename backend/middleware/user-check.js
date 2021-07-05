8a888ccc5d524a535c9af60b56239171955511ff
Jonathan Ranchin <jonathan.ranchin@yahoo.com>
2021-07-05 14:05:13 +0200

    Finalisation du user-check middleware

--- backend/middleware/user-check.js
+++ backend/middleware/user-check.js
@@ -5,23 +5,22 @@ const Sauce = require('../models/sauce');
 module.exports = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            if(sauce.userId !== req.body.userId) {
+            try {
+                const token = req.headers.authorization.split(" ")[1];
+                const decodedToken = jwt.verify(token, process.env.DB_PASS);
+                const userId = decodedToken.userId;
+                if(sauce.userId !== userId) {
                 throw "Invalid user ID";
+            } 
+            else {
+                next();
+            }
+            }  
+            catch {
+              res.status(403).json({
+                error: new Error("Invalid request!"),
+              });
             }
     })
-        .catch(error => res.status(400).json({ error }));
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.DB_PASS);
-    const userId = decodedToken.userId;
-    if (req.body.userId !== decodedToken.userId) {
-      throw "Invalid user ID";
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error("Invalid request!"),
-    });
-  }
+    
 };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -7,8 +7,8 @@ const multer = require('../middleware/multer-config');
 const idCheck = require('../middleware/user-check');
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put('/:id', idCheck, auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', idCheck, auth, sauceCtrl.deleteSauce);
+router.put('/:id', auth, multer, idCheck, sauceCtrl.modifySauce);
+router.delete('/:id', auth, idCheck, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.likeSauces);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
